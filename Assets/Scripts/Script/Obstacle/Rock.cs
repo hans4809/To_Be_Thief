34eb20530f09d40574695b43d17deb3d20951da0
rlaeodbsdk <147009763+rlaeodbsdk@users.noreply.github.com>
2023-11-04 02:29:55 +0900

    CCTV 불빛, 돌맹이 크기 변화
    Rocksize_level 의 값을 받아서 크기가 변화할 수 있게 설정했습니다.
    CCTV 불빛의 Z축으로 인해 불빛이 안보이는 문제 해결했습니다.

--- Assets/Scripts/Script/Obstacle/Rock.cs
+++ Assets/Scripts/Script/Obstacle/Rock.cs
@@ -7,14 +7,21 @@ public class Rock : MonoBehaviour
     public float RockSpeed;
     float originx;
 
+    public float Rocksize_level ;  // 크기 변수
+
     void Start()
     {
+        Rocksize_level = 0.75f; //  값의 변화를 조절 
+        transform.localScale = new Vector3(Rocksize_level, Rocksize_level, 3f); // 크기 설정
         RockSpeed = 5;
         originx = transform.position.x;
+   
+
     }
 
     void FixedUpdate()
     {
+     
         transform.position = transform.position + Vector3.right * RockSpeed * Time.deltaTime;
     }
 
