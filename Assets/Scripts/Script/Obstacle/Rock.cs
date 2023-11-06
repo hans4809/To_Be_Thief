36fc652a80c7237189c55f8f97f6131cb5754af4
yujeong430 <parkyj0430@gmail.com>
2023-11-06 16:02:48 +0900

    feat: 돌맹이 스폰 시간 변수 설정

--- Assets/Scripts/Script/Obstacle/Rock.cs
+++ Assets/Scripts/Script/Obstacle/Rock.cs
@@ -4,25 +4,36 @@ using UnityEngine;
 
 public class Rock : MonoBehaviour
 {
-    public float RockSpeed;
-    float originx;
+    public float RockSpeed_level; //이동속도 변수
+    public float Rockspawn_level; // 스폰시간 변수
+    public float Rocksize_level;  // 크기 변수
 
-    public float Rocksize_level ;  // 크기 변수
+    float originx;
+    float timer;
 
     void Start()
     {
         Rocksize_level = 0.75f; //  값의 변화를 조절 
         transform.localScale = new Vector3(Rocksize_level, Rocksize_level, 3f); // 크기 설정
-        RockSpeed = 5;
-        originx = transform.position.x;
-   
 
+        RockSpeed_level = 5;
+        originx = transform.position.x;
+        Rockspawn_level = 5;
+        timer = 0.0f;
+
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
     }
 
     void FixedUpdate()
     {
-     
-        transform.position = transform.position + Vector3.right * RockSpeed * Time.deltaTime;
+        if(timer > Rockspawn_level) //스폰시간 이후 움직임
+        {
+            transform.position = transform.position + Vector3.right * RockSpeed_level * Time.deltaTime;
+        }
     }
 
     void OnTriggerEnter2D(Collider2D collision)
@@ -30,6 +41,7 @@ public class Rock : MonoBehaviour
         if (collision.gameObject.tag == "Border")
         {
             transform.position = new Vector3(originx, transform.position.y, 0);
+            timer = 0; //화면 밖으로 나가면 timer 초기화
            
         }
         
