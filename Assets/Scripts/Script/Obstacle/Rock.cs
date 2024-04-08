dd64a7bcc73c22d1cfbdbfeda693f8e5b77329a9
yujeong <parkyj0430@gmail.com>
2024-04-08 16:13:47 +0900

    fix : 장애물 오류 수정
    1. 플레이어가 멈췄을 때 CCTV, 가시 장애물이 닿아도 게임오버 되지 않아서 수정했습니다.
    2. 돌맹이가 활성화 될 때마다 다른 패턴(진행 방향, 왕복 여부)을 가지도록 수정했습니다.

--- Assets/Scripts/Script/Obstacle/Rock.cs
+++ Assets/Scripts/Script/Obstacle/Rock.cs
@@ -13,8 +13,8 @@ public class Rock : MonoBehaviour
     float timer;
     float rotationTime;
 
-    int isReturn;
-    int isFlip;
+    public int isReturn;
+    public int isFlip;
 
     SpriteRenderer rend;
 
@@ -29,18 +29,6 @@ public class Rock : MonoBehaviour
         Rocksize_level = Managers.Data.currentStat[6]; //  값의 변화를 조절 , Sprite에 이미지 적용으로 값 조절
         RockSpeed_level = Managers.Data.currentStat[5];
         Rockspawn_level = Managers.Data.currentStat[4];
-        timer = 0.0f;
-        rotationTime = 0.0f;
-        isFlip = Random.Range(0, 2);
-        isReturn = Random.Range(0, 2);
-
-        if (isFlip == 1) // isFlip 1이면 오->왼
-        {
-            rend.flipX = true;
-            RockSpeed_level = -RockSpeed_level;
-            originx = -originx;
-            transform.position = new Vector3(originx, transform.position.y, 0);
-        }
     }
     void Update()
     {
@@ -56,18 +44,19 @@ public class Rock : MonoBehaviour
             rotation = Vector3.Lerp(new Vector3(0, 0, 0), new Vector3(0, 0, -359), rotationTime);
             transform.localEulerAngles = rotation;
         }
+
+        if (RockSpeed_level < 0) rend.flipX = true;
+        else rend.flipX = false;
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.name == "RReturn" && (isReturn == 1 && isFlip == 0)) //왼->오->왼 왕복
         {
-            rend.flipX = true;
             RockSpeed_level = -RockSpeed_level;
         }
         if (collision.gameObject.name == "LReturn" && (isReturn == 1 && isFlip == 1)) //오->왼->오 왕복
         {
-            rend.flipX = false;
             RockSpeed_level = -RockSpeed_level;
         }
         if (collision.gameObject.tag == "Border")
@@ -78,8 +67,6 @@ public class Rock : MonoBehaviour
             if(isReturn == 1) //1회 왕복 후 화면 밖으로 나갔을 때
             {
                 RockSpeed_level = -RockSpeed_level;
-                if (isFlip == 1) rend.flipX = true;
-                else rend.flipX = false;
             }
         }
         
@@ -90,6 +77,10 @@ public class Rock : MonoBehaviour
         RockSpeed_level = Managers.Data.currentStat[5];
         Rocksize_level = Managers.Data.currentStat[6];
         transform.localScale = new Vector3(Rocksize_level, Rocksize_level * 3.5f, 1);
+        timer = 0.0f;
+        rotationTime = 0.0f;
+        isFlip = Random.Range(0, 2);
+        isReturn = Random.Range(0, 2);
         if (isFlip == 1) // isFlip 1이면 오->왼
         {
             RockSpeed_level = -RockSpeed_level;
